e90f5061befd58038bdc061ebcb3ec9b2ab1752a
SophieTech88 <yun.sun7119@gmail.com>
2023-11-08 16:52:22 -0600

    task2

--- Task2:KinesisFirehoseWithJPExpression/KinesisFirehose-JPExpression.py
+++ Task2:KinesisFirehoseWithJPExpression/KinesisFirehose-JPExpression.py
@@ -4,20 +4,28 @@ import random
 import boto3
 import json
 
-## download Faker library first
-## pip install faker
 
 def get_data():
     f = Faker()
     table_list = ["table1", "table2", "table3"]
+    database_list = ["database1","database2"]
+    random_number_list = random.sample(range(1,6), 3)
+    print("random_number_list:",random_number_list)
     data = {
         "firstname" : f.unique.first_name(),
         "lastname" : f.unique.last_name(),
         "ssn" : f.unique.ssn(),
         "time" : str(datetime.now()),
-        "table" : random.choice(table_list)
+        "database" : random.choice(database_list),
+        "table" : random.choice(table_list),
+        "event_id" : random.choice(random_number_list)
     }
+    print(data)
     return data
+get_data()
+
+# s3 path: bucket_name/ingest/data/database/table/event_id/year/month/day/hour/file_name
+
 
 def put_record(payload = None, streamName=None):
     client = boto3.client("firehose")
@@ -27,7 +35,6 @@ def put_record(payload = None, streamName=None):
     )
 
 for i in range(20):
-    streamName = "SophieTech-KinesisFirehose-Demo"
+    streamName = "SophieTech-KinesisFirehose-Demo2"
     data = get_data()
-    print(data)
     put_record(payload = data, streamName=streamName)
